596e0db38ad023ca795278905513bb6bfa56ce11
jhonsuarez31 <86330565+jhonsuarez31@users.noreply.github.com>
2022-03-28 11:33:58 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log("Hola");
+
+const API_URL = "https://api.adviceslip.com/advice ";
+const title = document.getElementById("title");
+const queote = document.getElementById("queote");
+
+ 
+
+const queotef = async () => {
+  
+ 
+    const respuesta = await fetch(API_URL);
+    const datos = await respuesta.json();
+    advance(datos.slip.id, datos.slip.advice);
+
+    console.log(datos);
+   
+
+  
+}
+
+function advance(id, advice) {
+  title.textContent = `“ADIVICE # ${id}”`;
+  queote.textContent = `“${advice}”`;
+}
+
+
+
+
